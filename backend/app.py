47e144ad4240ace88b4c98e9b1cabd9b359f3330
sitayem <salehktaweel@gmail.com>
2024-09-17 10:49:20 +0300

    Set up Flask server

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import Config
 from extensions import db  # Import db from extensions.py
+from passlib.hash import argon2  # or use bcrypt
 
 def create_app():
     app = Flask(__name__)
@@ -10,11 +11,16 @@ def create_app():
     db.init_app(app)
     
     # Import models
-    from models import User
+    #from models import User
 
     @app.route('/')
     def index():
-        return "Flask server is running with password encryption!"
+        
+
+        # Hash a password for the first time
+        #hashed_password = argon2.hash("pass")
+        #print (hashed_password)
+        return "Flask server is running!"
 
     return app
 
--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,8 @@
+import os
+
 class Config(object):
     SECRET_KEY = '123'  # Replace with a secure key
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    # Ensure the database is stored in the instance folder (not root)
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "database.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
